f06d431e24361464945d822f91ee26b46951b1d5
Abdullah Khan <abdullahkh0615@gmail.com>
2023-12-05 11:44:46 -0500

    [DEV-6] Added shooting functionality. Added attribute enemies. Added accesiblity features

--- PirateModel/Entities/Player.java
+++ PirateModel/Entities/Player.java
@@ -1,12 +1,16 @@
 package PirateModel.Entities;
 
+import PirateModel.Accessibility.TextToSpeech;
 import PirateModel.MovementMediator;
 import PirateModel.PirateGame;
 import PirateModel.Ships.Ship;
 import PirateModel.TileContainer;
 
+import java.util.ArrayList;
+
 public class Player extends Entity{
 
+    public ArrayList<Entity> enemies;
 
     public Player(String ID, MovementMediator mover) {
         super(ID, mover);
@@ -24,17 +28,35 @@ public class Player extends Entity{
 
             String[] command = move.split(" ");
 
+            TextToSpeech tts = new TextToSpeech();
+
             switch (command[0]) {
                 case "MOVE" -> {
                     command_valid = move(command[1]);
+                    if (command_valid) {
+                        tts.speak("You moved!");
+                    } else {
+                        tts.speak("Invalid command!");
+                    }
                 }
                 case "FIRE" -> {
+                    if (enemies.isEmpty()) {
+                        tts.speak("No enemies on board!");
+                    }
+
+                    // in BattleController, add line for changing the Player.enemies attribute
+
+                    boolean shoot = this.shoot(enemies.get(0), Integer.parseInt(command[1]), Integer.parseInt(command[2]));
+
+                    if (shoot) {
+                        tts.speak("You hit the enemy!");
+
+                    } else {
+                        tts.speak("You missed!");
+                    }
 
                 }
                 case "RETREAT" -> {
-
-                }
-                default -> {
 
                 }
             }
