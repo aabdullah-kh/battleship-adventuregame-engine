31aa95562078f28bc15d9f57b33aeef42eed8e1d
Ramy Mina <ramy.mina0214@gmail.com>
2023-12-05 19:00:00 -0500

    [DEV-2] Fixed issue with all the constructors.

--- PirateModel/Ships/Ship.java
+++ PirateModel/Ships/Ship.java
@@ -1,32 +1,83 @@
 package PirateModel.Ships;
 
-public abstract class Ship {
+import PirateModel.Entities.Entity;
 
-    protected int xCoord;
+public class Ship {
 
-    protected int yCoord;
+    /**
+     * The name of the Ship.
+     */
+    private final String name;
 
-    protected String name;
+    /**
+     * The Ship's current health.
+     */
+    private int health;
 
-    protected int health;
+    /**
+     * The Ship cannons damage per shot.
+     */
+    private final int damage;
 
-    protected int damage;
 
-    public String getStats() {
-        throw new UnsupportedOperationException("Not implemented!");
+    /**
+     * Ship constructor.
+     * __________________________
+     * Initializes attributes.
+     *
+     * @param name:   the Ship's name.
+     * @param health: the Ship's health.
+     * @param damage: the Ship's damage.
+     */
+    public Ship(String name, int health, int damage) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
     }
 
-    public abstract boolean shoot(int x, int y);
-
-    public void modifyDamage(int damageBoostAmount) {
-        this.damage += damageBoostAmount;
+    /**
+     * Return the name of the Ship.
+     *
+     * @return String representation of the name.
+     */
+    public String getName() {
+        return this.name;
     }
 
-    public void modifyHealth(int healthIncrease) {
-        this.health += healthIncrease;
+    /**
+     * Add the Ship's health from the health argument.
+     *
+     * @param health: The health to be increased from the Ship's health
+     */
+    public void addHealth(int health) {
+        this.health += health;
     }
 
+    /**
+     * Subtract the Ship's health from the health argument.
+     *
+     * @param health: The health to be decreased from the Ship's health.
+     */
+    public void subtractHealth(int health) {
+        this.health -= health;
+    }
+
+    /**
+     * Return the Ship's damage per shot fired.
+     *
+     * @return int representation of the Ship's damage.
+     */
+    public int getDamage() {
+        return this.damage;
+    }
+
+    /**
+     * Return the Ship's current health.
+     *
+     * @return int representation of the Ship's health.
+     */
     public int getHealth() {
-        return  this.health;
+        return this.health;
     }
+
 }
